25abfdafc202c657325990fad69bdf5fb0d076fa
Burlen Loring <bloring@lbl.gov>
2018-11-08 14:27:24 -0800

    added cori demo

--- oscillator_python.sh
+++ oscillator_python.sh
@@ -3,10 +3,26 @@
 n=4
 b=64
 dt=0.05
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+bld=`echo -e '\e[1m'`
+red=`echo -e '\e[31m'`
+grn=`echo -e '\e[32m'`
+blu=`echo -e '\e[36m'`
+wht=`echo -e '\e[0m'`
 
-echo "+ module load sensei/2.1.1-vtk-shared"
-module load sensei/2.1.1-vtk-shared
+mkdir -p ./configs
+cp ${script_dir}/configs/* ./configs
+
+echo "+ module use /usr/common/software/sensei/modulefiles"
+module use /usr/common/software/sensei/modulefiles
+
+echo "+ module load sensei/2.1.0-vtk-shared"
+module load sensei/2.1.0-vtk-shared
 
 set -x
-mpiexec -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -p 0 \
-    -f ./configs/random_2d_${b}_python.xml ./configs/random_2d_${b}.osc
+
+cat ./configs/random_2d_${b}_python.xml | sed "s/.*/$blu&$wht/"
+
+srun -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -p 0 \
+    -f ./configs/random_2d_${b}_python.xml \
+    ./configs/random_2d_${b}.osc  2>&1 | sed "s/.*/$red&$wht/"
