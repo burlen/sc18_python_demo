ad5ec1ffec01d76558ab3cdaf0310bbfac95c1c8
SENSEI SC18 VM <sensei@sensei-insitu.org>
2018-11-09 09:08:10 -0800

    add color output to vm demo

--- oscillator_python.sh
+++ oscillator_python.sh
@@ -2,14 +2,20 @@
 
 n=4
 b=64
-dt=0.05
+dt=0.01
+bld=`echo -e '\e[1m'`
+red=`echo -e '\e[31m'`
+grn=`echo -e '\e[32m'`
+blu=`echo -e '\e[36m'`
+wht=`echo -e '\e[0m'`
 
 echo "+ module load sensei/2.1.1-vtk-shared"
 module load sensei/2.1.1-vtk-shared
 
 set -x
 
-cat ./configs/random_2d_${b}_python.xml
+cat ./configs/random_2d_${b}_python.xml | sed "s/.*/$blu&$wht/"
 
 mpiexec -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -p 0 \
-    -f ./configs/random_2d_${b}_python.xml ./configs/random_2d_${b}.osc
+    -f ./configs/random_2d_${b}_python.xml \
+    ./configs/random_2d_${b}.osc  2>&1 | sed "s/.*/$red&$wht/"
