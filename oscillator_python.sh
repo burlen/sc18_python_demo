f24dd81c2cd65f0adc8e842fdfb546bcbed19065
SENSEI SC18 VM <sensei@sensei-insitu.org>
2018-11-09 09:05:13 -0800

    print xml

--- oscillator_python.sh
+++ oscillator_python.sh
@@ -8,5 +8,8 @@ echo "+ module load sensei/2.1.1-vtk-shared"
 module load sensei/2.1.1-vtk-shared
 
 set -x
+
+cat ./configs/random_2d_${b}_python.xml
+
 mpiexec -n ${n} oscillator -b ${n} -t ${dt} -s ${b},${b},1 -p 0 \
     -f ./configs/random_2d_${b}_python.xml ./configs/random_2d_${b}.osc
